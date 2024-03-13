cc7dc02fcd9cfc51f8de4033fb881aad1596b53b
ZlodeyKovarniy <voboris0ff@gmail.com>
2024-03-13 16:56:04 +0300

    other

---
+++ Ashes of war patch 1.8.7/Ashes of war/Menu/Structure/maps/decoration.tsx
@@ -0,0 +1,4 @@
+<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
+<tileset firstgid="298" name="decoration" columns="7" tilecount="35" tileheight="20" tilewidth="20">
+  <image source="bitmaps/decoration.png" width="140" height="100" />
+</tileset>
---
+++ Ashes of war patch 1.8.7/Ashes of war/Menu/Structure/maps/misc.tsx
@@ -0,0 +1,50 @@
+<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
+<tileset firstgid="280" name="misc" columns="3" tilecount="18" tileheight="20" tilewidth="20">
+  <image source="bitmaps/misc.png" width="79" height="120" />
+  <tile id="4">
+    <properties>
+      <property name="res_pool" value="" />
+    </properties>
+  </tile>
+  <tile id="9">
+    <properties>
+      <property name="team" value="none" />
+      <property name="unit" value="tree" />
+    </properties>
+  </tile>
+  <tile id="10">
+    <properties>
+      <property name="team" value="none" />
+      <property name="type" value="1.0" />
+      <property name="unit" value="tree" />
+    </properties>
+  </tile>
+  <tile id="11">
+    <properties>
+      <property name="team" value="none" />
+      <property name="type" value="1.1" />
+      <property name="unit" value="tree" />
+    </properties>
+  </tile>
+  <tile id="12">
+    <properties>
+      <property name="team" value="none" />
+      <property name="type" value="1.2" />
+      <property name="unit" value="tree" />
+    </properties>
+  </tile>
+  <tile id="13">
+    <properties>
+      <property name="team" value="none" />
+      <property name="type" value="1.3" />
+      <property name="unit" value="tree" />
+    </properties>
+  </tile>
+  <tile id="14">
+    <properties>
+      <property name="team" value="none" />
+      <property name="type" value="1.4" />
+      <property name="unit" value="tree" />
+    </properties>
+  </tile>
+</tileset>
